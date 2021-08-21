0be77196ba541875b27614c2732fbe8df56891e8
duyn <khanhduy.dev@gmail.com>
2021-08-21 18:04:12 +0700

    intital commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  let items = document.getElementsByClassName("cards__item");
+  let list_items = document.getElementsByClassName("cards")[0];
+  list_items.addEventListener("click", (res) => {
+    if (res.target.classList[0] == "cards") {
+      let list = res.target.children;
+      for (let i = 0; i < list.length; i++) {
+        const e = list[i];
+        e.classList.remove("active");
+      }
+    }
+  });
+  for (const item of items) {
+    item.addEventListener("click", (res) => {
+      let list = res.path[1].children;
+      for (let i = 0; i < list.length; i++) {
+        const e = list[i];
+        e.classList.remove("active");
+      }
+      res.target.classList.add("active");
+    });
+  }
+});
