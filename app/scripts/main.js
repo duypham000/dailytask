db25cfdfeef8b56d22d7d38d63c65c5594165431
duyn <khanhduy.dev@gmail.com>
2021-08-22 22:44:51 +0700

    day 2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,53 @@
 $(document).ready(function () {
-  let items = document.getElementsByClassName("cards__item");
-  let list_items = document.getElementsByClassName("cards")[0];
-  list_items.addEventListener("click", (res) => {
-    if (res.target.classList[0] == "cards") {
-      let list = res.target.children;
-      for (let i = 0; i < list.length; i++) {
-        const e = list[i];
-        e.classList.remove("active");
-      }
-    }
+  changeProg(1);
+  let current = 1;
+
+  nxt.addEventListener("click", () => {
+    current++;
+    if (current > 4) current = 4;
+    else changeProg(current);
   });
-  for (const item of items) {
-    item.addEventListener("click", (res) => {
-      let list = res.path[1].children;
-      for (let i = 0; i < list.length; i++) {
-        const e = list[i];
-        e.classList.remove("active");
-      }
-      res.target.classList.add("active");
-    });
-  }
+  prv.addEventListener("click", () => {
+    current--;
+    if (current < 1) current = 1;
+    else changeProg(current);
+  });
 });
+
+const nxt = document.getElementsByClassName("prog__control-next")[0];
+const prv = document.getElementsByClassName("prog__control-prev")[0];
+
+const list = document.getElementsByClassName("prog__view-dot");
+const bar = document.getElementsByClassName("prog__view-bar")[0];
+
+function changeProg(node) {
+  for (let i = 0; i < list.length; i++) {
+    const ele = list[i];
+    ele.style.border = "#bdc3c7 0.3rem solid";
+  }
+  for (let i = 0; i < node; i++) {
+    const ele = list[i];
+    ele.style.border = "#2980b9 0.3rem solid";
+  }
+
+  prv.style = "";
+  nxt.style = "";
+  switch (node) {
+    case 1:
+      prv.style = "background: #bdc3c7;cursor: not-allowed;";
+      bar.style = "transform: translateX(-95%)";
+      break;
+    case 2:
+      bar.style = "transform: translateX(-65%)";
+      break;
+    case 3:
+      bar.style = "transform: translateX(-35%)";
+      break;
+    case 4:
+      nxt.style = "background: #bdc3c7;cursor: not-allowed;";
+      bar.style = "transform: translateX(-5%)";
+      break;
+    default:
+      break;
+  }
+}
