d2a7a33a3dceab0f6e2e37a61c543c535b9d597c
duyn <khanhduy.dev@gmail.com>
2021-08-25 23:30:21 +0700

    init day 4

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,10 @@
 $(document).ready(function () {
-  let stt = 0;
-  opBtn.addEventListener("click", () => {
-    if (stt == 0) {
-      stt = 1;
-      list.classList.add("list-actv");
-      ico.style = "transform: translate(-50%, -50%)";
-      ctn.style = "transform: rotate(-20deg)";
-    }
-  });
-  clsBtn.addEventListener("click", () => {
-    if (stt == 1) {
-      stt = 0;
-      list.classList.remove("list-actv");
-      ico.style = "transform: translate(-50%, -50%) rotate(80deg)";
-      ctn.style = "";
-    }
+  btn.addEventListener("click", () => {
+    if (active) inpt.classList.remove("active");
+    else inpt.classList.add("active");
+    active = !active;
   });
 });
-
-const ico = document.getElementsByClassName("slideMenu__icon")[0];
-const opBtn = document.getElementsByClassName("slideMenu__icon-open")[0];
-const clsBtn = document.getElementsByClassName("slideMenu__icon-close")[0];
-const list = document.getElementsByClassName("slideMenu__list")[0];
-const ctn = document.getElementsByClassName("content")[0];
+let active = true;
+const inpt = document.querySelector(".searchBar > input");
+const btn = document.querySelector(".searchBar > .btn");
