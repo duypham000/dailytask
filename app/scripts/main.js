93d7839f91a942dc96bc305b14bafae4500f220a
duyn <khanhduy.dev@gmail.com>
2021-08-27 18:35:47 +0700

    init day 5

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,26 @@
 $(document).ready(function () {
-  btn.addEventListener("click", () => {
-    if (active) inpt.classList.remove("active");
-    else inpt.classList.add("active");
-    active = !active;
-  });
+  let pros = 0;
+  let loop = setInterval(() => {
+    appear(pros);
+    pros++;
+    if (pros > 100) {
+      clearInterval(loop);
+    }
+  }, 50);
 });
-let active = true;
-const inpt = document.querySelector(".searchBar > input");
-const btn = document.querySelector(".searchBar > .btn");
+
+const wrp = document.getElementsByClassName("wrapper")[0];
+const blur = document.getElementsByClassName("blur")[0];
+const txt = document.querySelector(".blur > p");
+
+function appear(pros) {
+  let bl = 25 - pros / 4 - 0.25;
+  let opa = 1 - pros / 100;
+  txt.innerHTML = pros + "%";
+  if (pros == 100) {
+    blur.style = "display: none;";
+  } else {
+    wrp.style = "filter: blur(" + bl + "px);";
+    blur.style = "opacity:" + opa + ";";
+  }
+}
